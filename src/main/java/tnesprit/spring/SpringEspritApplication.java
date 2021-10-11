4abcaf8bd95c71278a83d915467b11d09ca54f29
FediAbdelkebir <abdelkebir.fedi@esprit.tn>
2021-10-11 10:18:25 +0100

    SpringBoot

---
+++ src/main/java/tnesprit/spring/SpringEspritApplication.java
@@ -0,0 +1,13 @@
+package tnesprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEspritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEspritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tnesprit/spring/SpringEspritApplicationTests.java
@@ -0,0 +1,13 @@
+package tnesprit.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEspritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
